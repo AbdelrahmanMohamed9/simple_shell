4d5c00ab041f2dccd6d7ca46308432063e6179d2
AbdelrahmanMohamed9 <abdelrahmanmohammed049@gmail.com>
2023-05-25 13:25:32 -0700

    new files

--- get_line.c
+++ get_line.c
@@ -1,49 +1,48 @@
 #include "shell.h"
 
+
 /**
- * input_buf - stores a sequence of commands in a buf.
+ * input_buffer - stores a sequence of commands in a buf.
  * @info: structure containing parameters.
- * @buf: memory location of the buf.
- * @len: memory location of the length variable.
+ * @buffer: memory location of the buf.
+ * @length: memory location of the length variable.
  *
  * Return:amount of data that has been read.
  */
-ssize_t input_buf(info_t *info, char **buf, size_t *len)
+
+ssize_t input_buffer(info_t *info, char **buffer, size_t *length)
 {
 	ssize_t v = 0;
-	size_t lP = 0;
+	size_t ln = 0;
 
-	while (!*len)
+	if (!*length)
 	{
 
-		free(*buf);
-		*buf = NULL;
-
+		free(*buffer);
+		*buffer = NULL;
 		signal(SIGINT, sigintHandler);
 #if USE_GETLINE
-		v = getline(buf, &lP, stdin);
+		v = getline(buffer, &ln, stdin);
 #else
-		v = _getline(info, buf, &lP);
+		v = _getline(info, buffer, &ln);
 #endif
-	if (v > 0)
-	{
-		if ((*buf)[v - 1] == '\n')
+		if (v > 0)
 		{
-			(*buf)[v - 1] = '\0';
-			v--;
-		}
-		info->linecount_flag = 1;
-		remove_comments(*buf);
-		build_history_list(info, *buf, info->histcount++);
+			if ((*buffer)[v - 1] == '\n')
+			{
+				(*buffer)[v - 1] = '\0';
+				v--;
+			}
+			info->linecount_flag = 1;
+			remove_comments(*buffer);
+			build_history_list(info, *buffer, info->histcount++);
 
-		while (_strchr(*buf, ';'))
-		{
-			*len = v;
-			info->cmd_buf = buf;
+			{
+				*length = v;
+				info->cmd_buf = buffer;
+			}
 		}
 	}
-	}
-
 	return (v);
 }
 
@@ -53,71 +52,64 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
  *
  * Return: the number of Bytes that have been Read.
  */
+
 ssize_t get_input(info_t *info)
 {
 	static char *buffer;
-	static size_t n, k, ln;
+	static size_t n, k, len;
 	ssize_t v = 0;
-	char **bp = &(info->arg), *b;
+	char **bf_p = &(info->arg), *b;
 
-	n = 0;
-	k = 0;
-	ln = 0;
 	_putchar(BUF_FLUSH);
-	while (1)
-	{	v = input_buf(info, &buffer, &ln);
-
-		if (v == -1)
+	v = input_buffer(info, &buffer, &len);
+	if (v == -1)
 		return (-1);
-		if (ln)
+	if (len)
+	{
+		k = n;
+		b = buffer + n;
+
+		check_chain(info, buffer, &k, n, len);
+		while (k < len)
 		{
-			k = n;
-			b = buffer + n;
-			check_chain(info, buffer, &k, n, ln);
-			while (k < ln)
-			{
-				if (is_chain(info, buffer, &k))
-					break;
-				k++;
-			}
-			n = k + 1;
-			if (n >= ln)
-			{
-				n = ln = 0;
-				info->cmd_buf_type = CMD_NORM;
-			}
-			*bp = b;
-			return (_strlen(b));
+			if (is_chain(info, buffer, &k))
+				break;
+			k++;
 		}
-		else
+
+		n = k + 1;
+		if (n >= len)
 		{
-			*bp = buffer;
-			return (v);
+			n = len = 0;
+			info->cmd_buf_type = CMD_NORM;
 		}
+
+		*bf_p = b;
+		return (_strlen(b));
 	}
+
+	*bf_p = buffer;
+	return (v);
 }
 
 /**
- * read_buf - copies data from a buf.
+ * read_buffer - copies data from a buf.
  * @info: structure containing parameters.
- * @buf:That Is The buf.
+ * @buffer:That Is The buf.
  * @i:That Is The Size.
  *
  * Return: V.
  */
-ssize_t read_buf(info_t *info, char *buf, size_t *i)
+
+ssize_t read_buffer(info_t *info, char *buffer, size_t *i)
 {
 	ssize_t v = 0;
 
-	while (!(*i))
-	{
-		v = read(info->readfd, buf, READ_BUF_SIZE);
-		if (v >= 0)
+	if (*i)
+		return (0);
+	v = read(info->readfd, buffer, READ_BUF_SIZE);
+	if (v >= 0)
 		*i = v;
-		else
-			break;
-	}
-
 	return (v);
 }
 
@@ -129,56 +121,52 @@ ssize_t read_buf(info_t *info, char *buf, size_t *i)
  *
  * Return: c.
  */
-int _getline(info_t *info, char **ptr, size_t *length)
+
+int _getline(info_t *info, char **p, size_t *len)
 {
-	static char buffer[READ_BUF_SIZE];
+	static char buf[READ_BUF_SIZE];
 	static size_t n, ln;
-	size_t t = 0;
+	size_t t;
 	ssize_t v = 0, c = 0;
-	char *b = NULL, *np = NULL, *x;
+	char *b = NULL, *n_p = NULL, *x;
 
-	n = 0;
-	ln = 0;
+	b = *p;
+	if (b && len)
+		c = *len;
+	if (n == ln)
+		n = ln = 0;
 
-	b = *ptr;
+	v = read_buffer(info, buf, &ln);
+	if (v == -1 || (v == 0 && ln == 0))
+		return (-1);
 
-	if (b && length)
-		c = *length;
-
-	while (n == ln)
-	{
-		v = read_buf(info, buffer, &ln);
-
-		if (v == -1 || (v == 0 && ln == 0))
-			return (-1);
-		n = 0;
-	}
-
-	x = _strchr(buffer + n, '\n');
-	t = x ? 1 + (unsigned int)(x - buffer) : ln;
-	np = _realloc(b, c, c ? c + t : t + 1);
-
-	if (!np)
+	x = _strchr(buf + n, '\n');
+	t = x ? 1 + (unsigned int)(x - buf) : ln;
+	n_p = _realloc(b, c, c ? c + t : t + 1);
+	if (!n_p)
 		return (b ? free(b), -1 : -1);
+
 	if (c)
-		_strncat(np, buffer + n, t - n);
+		_strncat(n_p, buf + n, t - n);
 	else
-		_strncpy(np, buffer + n, t - n + 1);
+		_strncpy(n_p, buf + n, t - n + 1);
+
 	c += t - n;
 	n = t;
-	b = np;
-	if (length)
-		*length = c;
-	*ptr = b;
+	b = n_p;
+
+	if (len)
+		*len = c;
+	*p = b;
 	return (c);
 }
 
+
 /**
- * sigintHandler - blocks ctrl-C
+ * sigintHandler - sigintHandler - blocks ctrl-C
  * @sig_num: the signal number
- *
- * Return: void
  */
+
 void sigintHandler(__attribute__((unused))int sig_num)
 {
 	_puts("\n");
